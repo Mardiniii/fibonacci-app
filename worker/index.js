819a6a568d2360dcd6e17b43beb3b305107cf5d1
mardiniii <sezama03@hotmail.com>
2019-03-16 14:04:44 -0400

    Add Node.js service (Worker).

---
+++ worker/index.js
@@ -0,0 +1,21 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+
+// We are using this recursion approach because we want a slow function performance
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
