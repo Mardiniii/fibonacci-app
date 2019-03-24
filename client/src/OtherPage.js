d8b6973d71440fd5ec8d8d5531008a4ff73acd43
mardiniii <sezama03@hotmail.com>
2019-03-23 21:25:00 -0400

    Upgrade react-router-dom to version 5.0.0.

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      Im some other page      
+      Im some other page!
+      <Link to="/">Go back home</Link>
     </div>
   );
 };
